26c16c98561938272971bfe8e64cc5c61035a44c
sohan <sohanprasad2003@gmail.com>
2025-12-07 12:11:54 +0530

    create first project

---
+++ Sample-1/SampleBank/src/main/java/com/example/SampleBank/SampleBankApplication.java
@@ -0,0 +1,13 @@
+package com.example.SampleBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleBankApplication.class, args);
+	}
+
+}
