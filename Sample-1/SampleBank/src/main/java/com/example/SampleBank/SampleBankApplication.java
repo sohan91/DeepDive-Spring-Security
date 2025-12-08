a91596f126755b647d79fb40cc4517c4a33ea0c2
sohan <sohanprasad2003@gmail.com>
2025-12-08 13:27:08 +0530

    does some work

--- Sample-1/SampleBank/src/main/java/com/example/SampleBank/SampleBankApplication.java
+++ Sample-1/SampleBank/src/main/java/com/example/SampleBank/SampleBankApplication.java
@@ -2,8 +2,10 @@ package com.example.SampleBank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.example.SampleBank.controller")
 public class SampleBankApplication {
 
 	public static void main(String[] args) {
