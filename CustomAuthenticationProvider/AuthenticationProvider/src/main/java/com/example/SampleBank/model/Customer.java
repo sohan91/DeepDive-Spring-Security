23e81c096fa93331c9b329ad099cd7480b83d46b
sohan <sohanprasad2003@gmail.com>
2025-12-19 12:14:14 +0530

    implement custom authentication provider

---
+++ CustomAuthenticationProvider/AuthenticationProvider/src/main/java/com/example/SampleBank/model/Customer.java
@@ -0,0 +1,25 @@
+package com.example.SampleBank.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "customer")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "role", nullable = false)
+    private String role;
+}
